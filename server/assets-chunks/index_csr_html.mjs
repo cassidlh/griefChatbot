2acc033243a42b09cbfeb4112b74981cf63951bc
Cassidy Ho <cassidyleho@gmail.com>
2025-02-25 23:40:36 -0800

    Auto-generated commit

---
+++ server/assets-chunks/index_csr_html.mjs
@@ -0,0 +1,18 @@
+export default `<!doctype html>
+<html lang="en" data-beasties-container="">
+<head>
+  <meta charset="utf-8">
+  <title>AiChatbot</title>
+  <base href="/">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/x-icon" href="favicon.ico">
+
+  <style>
+    @import url('https://fonts.googleapis.com/css2?family=Urbanist:ital,wght@0,100..900;1,100..900&display=swap');
+    </style>
+<style>body{background-color:#8eadaa;color:#fff}</style><link rel="stylesheet" href="styles-YBSM3WPD.css" media="print" onload="this.media='all'"><noscript><link rel="stylesheet" href="styles-YBSM3WPD.css"></noscript></head>
+<body ngcm="">
+  <app-root></app-root>
+<script src="polyfills-FFHMD2TL.js" type="module"></script><script src="main-TB7FTILU.js" type="module"></script></body>
+</html>
+`;
